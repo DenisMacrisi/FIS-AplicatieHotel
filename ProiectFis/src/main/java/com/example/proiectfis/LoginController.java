55aa81f68edf7e65bc87d1684d1b50092759ef3d
irislung <61490848+irislung@users.noreply.github.com>
2022-05-18 18:45:18 +0300

    Update LoginController.java

--- ProiectFis/src/main/java/com/example/proiectfis/LoginController.java
+++ ProiectFis/src/main/java/com/example/proiectfis/LoginController.java
@@ -22,7 +22,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ResourceBundle;
 
-public class LoginController implements Initializable{
+public class LoginController implements Initializable {
     @FXML
     private Button cancelButton;
     @FXML
@@ -35,62 +35,113 @@ public class LoginController implements Initializable{
     private TextField usernameTextField;
     @FXML
     private PasswordField enterPasswordField;
-
-    public void initialize(URL url, ResourceBundle resourceBundle){
-        File brandingFile =new File("images/4.jpg");
-        Image brandingImage= new Image(brandingFile.toURI().toString());
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button regButton;
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        File brandingFile = new File("images/4.jpg");
+        Image brandingImage = new Image(brandingFile.toURI().toString());
         brandingImageView.setImage(brandingImage);
 
-        File lockFile =new File("images/lock.jpg");
-        Image lockImage= new Image(lockFile.toURI().toString());
+        File lockFile = new File("images/lock.jpg");
+        Image lockImage = new Image(lockFile.toURI().toString());
         lockImageView.setImage(lockImage);
     }
-    public void loginButtonOnAction(ActionEvent event){
+
+    public void loginButtonOnAction(ActionEvent event) {
 
         loginMessageLabel.setText("You tried to login");
-        if(usernameTextField.getText().isBlank()==false && enterPasswordField.getText().isBlank()==false){
+        if (usernameTextField.getText().isBlank() == false && enterPasswordField.getText().isBlank() == false) {
             validateLogin();
-        }else {
+
+        } else {
             loginMessageLabel.setText("Please enter your surname and password");
         }
     }
-    public void cancelButtonOnAction(ActionEvent event){
-        Stage stage=(Stage) cancelButton.getScene().getWindow();
+
+    public void cancelButtonOnAction(ActionEvent event) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
         stage.close();
     }
-    public void regButtonOnAction(ActionEvent event){
-        createAccountForm();
-    }
-    public void validateLogin(){
-        DatabaseConnection connectNow=new DatabaseConnection();
-        Connection connectDB=connectNow.getConnection();
-        String verifyLogin="SELECT count(1) FROM user_account WHERE userName='"+usernameTextField.getText()+"' and password ='"+enterPasswordField.getText()+"'";
 
+    public void regButtonOnAction(ActionEvent event) {
+        createAccountForm();
+        Stage stage=(Stage) regButton.getScene().getWindow();
+        stage.close();
+
+    }
+
+    public void validateLogin() {
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
+        String tip="admin";
+        String tip2="user";
+        String verifyLoginAdmin = "SELECT count(1) FROM user_account WHERE username='" + usernameTextField.getText() + "' and password ='" + enterPasswordField.getText() + "'and account_type='" + tip + "'";
+        String verifyLoginUser = "SELECT count(1) FROM user_account WHERE username='" + usernameTextField.getText() + "' and password ='" + enterPasswordField.getText() + "'and account_type='" + tip2 + "'";
         try {
-            Statement statement =connectDB.createStatement();
-            ResultSet queryResult=statement.executeQuery(verifyLogin);
-            while(queryResult.next()){
-                if(queryResult.getInt(1)==1){
+            Statement statement = connectDB.createStatement();
+            Statement statement1= connectDB.createStatement();
+            ResultSet queryResultAdmin = statement.executeQuery(verifyLoginAdmin);
+            ResultSet queryResultUser = statement1.executeQuery(verifyLoginUser);
+
+            while (queryResultAdmin.next() && queryResultUser.next()) {
+                if (queryResultAdmin.getInt(1) == 1) {
                     loginMessageLabel.setText("You are logged in");
-                }else {
-                    loginMessageLabel.setText("Invalid login");
+                    goToHomepageAdmin();
+                    Stage stage=(Stage) loginButton.getScene().getWindow();
+                    stage.close();
+                } else {
+                    if(queryResultUser.getInt(1)==1){
+                        goToHomepageUser();
+                        Stage stage=(Stage) loginButton.getScene().getWindow();
+                        stage.close();
+                    }else{
+                        loginMessageLabel.setText("Invalid login");
+                    }
                 }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             e.getCause();
         }
     }
-    public void createAccountForm(){
-        try{
-            Parent root= FXMLLoader.load(getClass().getResource("register.fxml"));
-            Stage registerStage=new Stage();
+
+    public void createAccountForm() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("register.fxml"));
+            Stage registerStage = new Stage();
             registerStage.initStyle(StageStyle.UNDECORATED);
-            registerStage.setScene(new Scene(root,520,554));
+            registerStage.setScene(new Scene(root, 520, 554));
             registerStage.show();
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             e.getCause();
         }
     }
+    public void goToHomepageAdmin(){
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("homepageAdmin.fxml"));
+            Stage registerStage = new Stage();
+            registerStage.initStyle(StageStyle.UNDECORATED);
+            registerStage.setScene(new Scene(root, 1000, 692));
+            registerStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
     }
+    public void goToHomepageUser(){
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("homepageUser.fxml"));
+            Stage registerStage = new Stage();
+            registerStage.initStyle(StageStyle.UNDECORATED);
+            registerStage.setScene(new Scene(root, 1000, 692));
+            registerStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+}
+
